24041dca6568e5316824dde3e617e3b77ed596dc
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2021-12-26 17:27:55 +0530

    Checking Fare For Premium Data

--- src/main/java/com/services/Invoice_Generator.java
+++ src/main/java/com/services/Invoice_Generator.java
@@ -11,44 +11,76 @@ public class Invoice_Generator {
 	public static final double COSTPerMIN = 1.0;
 	public static final double MIN_FARE = 5;
 
-	 // method to check the fare and totalFare < 5 then use the minfare data
-	public double calculateFare(double distance, int time) {
-		double totalFare = distance * COSTPerKM + time * COSTPerMIN;
-		if (totalFare < 5) {
-			return MIN_FARE;
+	public static final double PREMIUMCOSTPerKM = 15.0;
+	public static final double PREMIUMCOSTPerMIN = 2.0;
+	public static final double PREMIUMMIN_FARE = 20;
+
+	// method to check the fare and totalFare < 5 then use the minfare data
+	public double calculateFare(double distance, int time, String type) {
+		if (type == "Normal") {
+			double totalFare = distance * COSTPerKM + time * COSTPerMIN;
+			if (totalFare < 5) {
+				return MIN_FARE;
+			}
+			return totalFare;
+		} else {
+			double totalFare = distance * PREMIUMCOSTPerKM + time * PREMIUMCOSTPerMIN;
+			if (totalFare < 20) {
+				return PREMIUMMIN_FARE;
+			}
+			return totalFare;
 		}
-		return totalFare;
-	}
-	
-	//method to check the multiple ride fare
-	public CabInvoiceData calculateFareForMultiple_Ride(Ride[] rides) {
-		double totalFare = 0;
-		for(int i = 0; i < rides.length; i++) {
-			Ride ride = rides[i];
-			double fare = ride.getDistance() * COSTPerKM + ride.getTime() * COSTPerMIN;
-			totalFare += fare;
-		}
-		return new CabInvoiceData(rides.length,totalFare);
 	}
 
-	public CabInvoiceData cabSummaryCalculate(int userId) {
-		Map<Integer, Ride[]> map = new HashMap<>();
-		Ride[] rides = {
-				new Ride(2.0,5),
-				new Ride(0.1,1)
-		};
-		Ride[] rides1 = {
-				new Ride(2.0,5),
-				new Ride(0.1,1)
-		};
-		map.put(3, rides);
-		map.put(1, rides1);
-		for(Map.Entry<Integer, Ride[]> entry : map.entrySet()) {
-			if(userId == entry.getKey()) {
-				Ride[] data = entry.getValue();
-				return calculateFareForMultiple_Ride(data);
+	// method to check the multiple ride fare
+	public CabInvoiceData calculateFareForMultiple_Ride(Ride[] rides, String type) {
+		if (type == "Normal") {
+			double totalFare = 0;
+			for (int i = 0; i < rides.length; i++) {
+				Ride ride = rides[i];
+				double fare = ride.getDistance() * COSTPerKM + ride.getTime() * COSTPerMIN;
+				totalFare += fare;
 			}
+			return new CabInvoiceData(rides.length, totalFare);
+		} else {
+			double totalFare = 0;
+			for (int i = 0; i < rides.length; i++) {
+				Ride ride = rides[i];
+				double fare = ride.getDistance() * PREMIUMCOSTPerKM + ride.getTime() * PREMIUMCOSTPerMIN;
+				totalFare += fare;
+			}
+			return new CabInvoiceData(rides.length, totalFare);
 		}
-		return null;
+	}
+	
+	//Calculate Fare for Total Daily Rides Of One User
+	public CabInvoiceData cabSummaryCalculate(int userId, String type) {
+		if (type == "Normal") {
+			Map<Integer, Ride[]> map = new HashMap<>();
+			Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+			Ride[] rides1 = { new Ride(2.0, 5), new Ride(1.0, 1) };
+			map.put(3, rides);
+			map.put(1, rides1);
+			for (Map.Entry<Integer, Ride[]> entry : map.entrySet()) {
+				if (userId == entry.getKey()) {
+					Ride[] data = entry.getValue();
+					return calculateFareForMultiple_Ride(data, type);
+				}
+			}
+			return null;
+		} else {
+			Map<Integer, Ride[]> map = new HashMap<>();
+			Ride[] rides = { new Ride(2.0, 5), new Ride(1.0, 1) };
+			Ride[] rides1 = { new Ride(2.0, 5), new Ride(0.1, 1) };
+			map.put(3, rides);
+			map.put(1, rides1);
+			for (Map.Entry<Integer, Ride[]> entry : map.entrySet()) {
+				if (userId == entry.getKey()) {
+					Ride[] data = entry.getValue();
+					return calculateFareForMultiple_Ride(data, type);
+				}
+			}
+			return null;
+		}
 	}
 }
--- src/test/java/com/test/Invoice_Test.java
+++ src/test/java/com/test/Invoice_Test.java
@@ -8,45 +8,81 @@ import com.entity.CabInvoiceData;
 import com.entity.Ride;
 import com.services.Invoice_Generator;
 
-import junit.framework.Assert;
-
 public class Invoice_Test {
 	Invoice_Generator invoiceGenerator = new Invoice_Generator();
-	
+
 	// test method to check the fare return through the method data
-    @Test
-    public void givenDistanceAndTime_ShouldReturnFare(){
-        double distance = 2.0;
-        int time = 5;
-        double result = invoiceGenerator.calculateFare(distance,time);
-        assertEquals(25,result,0.0);
-    }
-    
-    // test method check the minfare data
-    @Test
-    public void givenDistanceAndTime_ShouldReturnMinFare(){
-        double distance = 0.1;
-        int time = 1;
-        double result = invoiceGenerator.calculateFare(distance,time);
-        assertEquals(5.0,result,0.0);
-    }
-    
-    //test method check the multiple ride data
-    @Test
-    public void givenMultipleRide_ShouldReturnFare() {
-    	Ride[] rides = {
-    		new Ride(2.0,5),
-    		new Ride(0.1,1)
-    	};
-    	CabInvoiceData result = invoiceGenerator.calculateFareForMultiple_Ride(rides);
-    	CabInvoiceData expectedData = new CabInvoiceData(2,27.0); 
-        assertEquals(result,expectedData);
-    }
-    
-    @Test
-    public void givenUserID_ShouldReturnInvoiceData() {
-    	CabInvoiceData cabInvoiceData = invoiceGenerator.cabSummaryCalculate(3);
-    	CabInvoiceData expectedResult = new CabInvoiceData(2, 27.0);
-    	assertEquals(cabInvoiceData.getInvoiceData(),expectedResult.getInvoiceData());
-    }
+	@Test
+	public void givenDistanceAndTime_ShouldReturnFare() {
+		double distance = 2.0;
+		int time = 5;
+		String type = "Normal";
+		double result = invoiceGenerator.calculateFare(distance, time, type);
+		assertEquals(25, result, 0.0);
+	}
+
+	// test method to check the fare return through the method data For Premium Data
+	@Test
+	public void givenDistanceAndTime_PremiumData_ShouldReturnFare() {
+		double distance = 2.0;
+		int time = 5;
+		String type = "Premium";
+		double result = invoiceGenerator.calculateFare(distance, time, type);
+		assertEquals(40, result, 0.0);
+	}
+
+	// test method check the minfare data
+	@Test
+	public void givenDistanceAndTime_ShouldReturnMinFare() {
+		double distance = 0.1;
+		int time = 1;
+		String type = "Normal";
+		double result = invoiceGenerator.calculateFare(distance, time, type);
+		assertEquals(5.0, result, 0.0);
+	}
+
+	// test method check the minfare data for Premium Data
+	@Test
+	public void givenDistanceAndTime_PremiumData_ShouldReturnMinFare() {
+		double distance = 0.1;
+		int time = 1;
+		String type = "Premium";
+		double result = invoiceGenerator.calculateFare(distance, time, type);
+		assertEquals(20.0, result, 0.0);
+	}
+
+	// test method check the multiple ride data
+	@Test
+	public void givenMultipleRide_ShouldReturnFare() {
+		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+		String type = "Normal";
+		CabInvoiceData result = invoiceGenerator.calculateFareForMultiple_Ride(rides, type);
+		CabInvoiceData expectedData = new CabInvoiceData(2, 27.0);
+		assertEquals(result, expectedData);
+	}
+
+	@Test
+	public void givenMultipleRide_PremiumData_ShouldReturnFare() {
+		Ride[] rides = { new Ride(2.0, 5), new Ride(1.0, 1) };
+		String type = "Premium";
+		CabInvoiceData result = invoiceGenerator.calculateFareForMultiple_Ride(rides, type);
+		CabInvoiceData expectedData = new CabInvoiceData(2, 57.0);
+		assertEquals(result, expectedData);
+	}
+
+	@Test
+	public void givenUserID_ShouldReturnInvoiceData() {
+		String type = "Normal";
+		CabInvoiceData cabInvoiceData = invoiceGenerator.cabSummaryCalculate(3, type);
+		CabInvoiceData expectedResult = new CabInvoiceData(2, 27.0);
+		assertEquals(cabInvoiceData.getInvoiceData(), expectedResult.getInvoiceData());
+	}
+
+	@Test
+	public void givenUserID_PremiumData_ShouldReturnInvoiceData() {
+		String type = "Premium";
+		CabInvoiceData cabInvoiceData = invoiceGenerator.cabSummaryCalculate(3, type);
+		CabInvoiceData expectedResult = new CabInvoiceData(2, 57.0);
+		assertEquals(cabInvoiceData.getInvoiceData(), expectedResult.getInvoiceData());
+	}
 }
